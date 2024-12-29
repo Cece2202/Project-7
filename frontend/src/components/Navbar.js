038e3311b8002024f72e25141600b0dafd8037e1
Christin Parker <cparker4400@gmail.com>
2024-12-29 11:27:00 -0600

    update the nav bar to show/hide menu items depending if the user is logged in or not

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/Navbar.css"; // Add this line to import the CSS file
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -15,10 +21,18 @@ const Navbar = () => {
       <div className="container">
         <Link className="navbar-brand" to="/">9Gang</Link>
         <div className="navbar-nav">
-        <Link className="nav-link" to="/signup">Signup</Link>
-        <Link className="nav-link" to="/login">Login</Link>
-        <Link className="nav-link" to="/profile">Profile</Link>
-        <button className="logout-btn" onClick={handleLogout}>Logout</button>
+        <Link className="nav-link" to="/home">Home</Link>
+        {isLoggedIn ? (
+          <>
+            <li><Link to="/forum">Forum</Link></li>
+            <li><button onClick={handleLogout}>Logout</button></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </>
+        )}
         </div>
       </div>
     </nav>
